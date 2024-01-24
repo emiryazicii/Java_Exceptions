85cd1396361dfc41a9291f5bd9767bba33ab1800
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:24:22 -0500

    Add some examples to explain how try catch block works.

--- src/exceptions_in_Java/TryCatchBlocks.java
+++ src/exceptions_in_Java/TryCatchBlocks.java
@@ -1,4 +1,47 @@
 package exceptions_in_Java;
 
 public class TryCatchBlocks {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program started");
+
+        try{
+            System.out.println(9/0); // Arithmetic exception
+            System.out.println("Try Block");
+        }catch (ArithmeticException e){
+            System.out.println("Catch Block");
+        }
+
+        System.out.println("Program ended");
+
+        System.out.println("-------------------------------------------------------------------------");
+
+        System.out.println("Program 2 started");
+
+        String str = null;
+
+        try{
+            System.out.println(str.toLowerCase());
+            System.out.println("Try Block");
+        }catch (RuntimeException e){
+            System.out.println("Catch Block");
+        }
+
+        System.out.println("Program 2 ended");
+
+        System.out.println("----------------------------------------------------------------------------");
+
+        System.out.println("Program 3 started");
+
+        try{
+            Thread.sleep(5000);
+            System.out.println("Try Block");
+        }catch (InterruptedException e){
+
+            System.out.println("Catch Block");
+        }
+
+        System.out.println("Program 3 ended");
+    }
 }

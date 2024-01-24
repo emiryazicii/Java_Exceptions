3df86bff57a06cdb123bb5ddd6e203e8d26f9ba5
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:49:40 -0500

    Make InvalidShapeException class as a child of RuntimeException class .

--- src/exceptions_in_Java/shapeTask/InvalidShapeException.java
+++ src/exceptions_in_Java/shapeTask/InvalidShapeException.java
@@ -1,4 +1,11 @@
 package exceptions_in_Java.shapeTask;
 
-public class InvalidShapeException {
+public class InvalidShapeException extends RuntimeException{
+
+
+    public InvalidShapeException(String message){
+
+        super(message);
+
+    }
 }

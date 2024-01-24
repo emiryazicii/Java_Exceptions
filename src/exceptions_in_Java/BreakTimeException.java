b9a546d2481f6ba29efd8f0c4db61315c1010780
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:47:10 -0500

    Make BreakTimeException class as a child of RuntimeException class and add an example to practice.

--- src/exceptions_in_Java/BreakTimeException.java
+++ src/exceptions_in_Java/BreakTimeException.java
@@ -1,4 +1,15 @@
 package exceptions_in_Java;
 
-public class BreakTimeException {
+public class BreakTimeException extends RuntimeException{
+
+
+    public BreakTimeException() {
+
+        super("It's break time");
+    }
+
+    public BreakTimeException(String message){
+
+        super(message);
+    }
 }

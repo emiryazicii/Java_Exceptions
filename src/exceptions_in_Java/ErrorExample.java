a84e58056ce325c5cfc132010833e01360254d08
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:23:13 -0500

    Add an example to display how error occurs.

--- src/exceptions_in_Java/ErrorExample.java
+++ src/exceptions_in_Java/ErrorExample.java
@@ -1,4 +1,15 @@
 package exceptions_in_Java;
 
+import java.util.ArrayList;
+
 public class ErrorExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+
+        while(true){
+            pizzas.add( new Pizza('S',2,3));
+        }
+    }
 }

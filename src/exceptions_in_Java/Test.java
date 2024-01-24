07ffe1faad82d5f734923f09a5c323091ebd1044
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:56:32 -0500

    Add some examples to practice.

--- src/exceptions_in_Java/Test.java
+++ src/exceptions_in_Java/Test.java
@@ -1,4 +1,35 @@
 package exceptions_in_Java;
 
+import library_test.Library;
+
+import java.time.LocalTime;
+
 public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+
+        //Thread.sleep(3500);
+
+        Library.sleep(3.5);
+
+        System.out.println("How are you today ?");
+
+
+        System.out.println("-----------------------------------------------------------------------");
+
+
+        if(LocalTime.now().equals(LocalTime.of(4,0))){
+            throw new BreakTimeException();
+
+        }
+
+        //throw new RuntimeException("Something went wrong");
+
+        throw new BreakTimeException("It's time to go home");
+
+        //throw new BreakTimeException();
+
+    }
 }

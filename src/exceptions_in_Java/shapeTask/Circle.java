82947b6ff03cadc2067137fecf2bfd14423896bf
European01 <emiryaziciofficial@gmail.com>
2024-01-23 19:50:31 -0500

    Add an example to practice.

--- src/exceptions_in_Java/shapeTask/Circle.java
+++ src/exceptions_in_Java/shapeTask/Circle.java
@@ -1,4 +1,21 @@
 package exceptions_in_Java.shapeTask;
 
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            throw new InvalidShapeException("Radius of the circle can't be zero or negative");
+        }
+        this.radius = radius;
+    }
 }

6791775ec24e633cdf86cc675b9aa4f807c413b3
European01 <emiryaziciofficial@gmail.com>
2024-02-06 01:20:17 -0500

    Create an custom exception.

--- src/exceptions_in_Java/practices/NotEnoughOxygenException.java
+++ src/exceptions_in_Java/practices/NotEnoughOxygenException.java
@@ -1,4 +1,9 @@
 package exceptions_in_Java.practices;
 
-public class NotEnoughOxygenException {
+public class NotEnoughOxygenException extends Exception{
+
+    public NotEnoughOxygenException(){
+
+        super("O2 levels low");
+    }
 }
